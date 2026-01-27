771e22b17f242d47721b533b8b50202de7029211
NatuAls <nahuelalesso19@gmail.com>
2026-01-27 02:35:50 +0100

    fixed name filtering v2

--- api/src/controllers/getApiDogs.js
+++ api/src/controllers/getApiDogs.js
@@ -11,7 +11,7 @@ module.exports = getApiDogs = async (name = null) => {
                 name: e.name,
                 weight: e.weight.metric.replace('NaN', 'Sin especificar'),
                 temperament: e.temperament,
-                image: e.image?.url || (e.reference_image_id ? `https://cdn.thedogapi.com/images/${e.reference_image_id}.jpg` : null)
+                image: e.image?.url || (e.reference_image_id ? `https://cdn4.thedogapi.com/optimized/${e.reference_image_id}.jpg` : null)
             }
         }));
 
@@ -22,29 +22,16 @@ module.exports = getApiDogs = async (name = null) => {
             .then(response => response.data)
             .then(data => {
                 if(!data.length) return data;
-                return  data.map(e => {
-                    if(e.reference_image_id){
-                        return {
-                            id: e.id,
-                            name: e.name,
-                            height: e.height.metric.replace('NaN', 'Sin especificar'),
-                            weight: e.weight.metric.replace('NaN', 'Sin especificar'),
-                            life_span: e.life_span.replace('year', 'años'),
-                            temperament: e.temperament,
-                            image: `https://cdn.thedogapi.com/images/${e.reference_image_id}.jpg`
-                        }
-                    }
-                    else{
-                        return {
-                            id: e.id,
-                            name: e.name,
-                            height: e.height.metric.replace('NaN', 'Sin especificar'),
-                            weight: e.weight.metric.replace('NaN', 'Sin especificar'),
-                            life_span: e.life_span.replace('year', 'años'),
-                            temperament: e.temperament,
-                            image: null
-                        }
-                    }
+                return data.map(e => {
+                    return {
+                        id: e.id,
+                        name: e.name,
+                        height: e.height.metric.replace('NaN', 'Sin especificar'),
+                        weight: e.weight.metric.replace('NaN', 'Sin especificar'),
+                        life_span: e.life_span.replace('year', 'años'),
+                        temperament: e.temperament,
+                        image: e.image?.url || null
+                    };
                 });
             });
     }
